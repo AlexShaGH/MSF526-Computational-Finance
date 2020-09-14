0bcd958e555dfb9bdd04bc01b80f03ebb79e8421
AlexShaGH <70188803+AlexShaGH@users.noreply.github.com>
2020-09-14 01:44:23 -0500

    progress

--- Homework-1/Bisect.py
+++ Homework-1/Bisect.py
@@ -12,8 +12,8 @@
 __author__ = "oshashkov"
 
 import math
-from math import log, sqrt, exp
-from typing import Callable, Iterator, Union, Optional, List
+#from math import log, sqrt, exp
+#from typing import Callable, Iterator, Union, Optional, List
 
 def newton(target, function, derivfun, start, bounds=None,
            tols=[0.001, 0.010], maxiter=1000):
@@ -48,7 +48,13 @@ def newton(target, function, derivfun, start, bounds=None,
     fdiffs : [float,float,...]
         the set of (target - y) values at the xvals[]
     """
-    # TODO: check input parameters
+    # check input parameters
+    if math.isnan(target):
+        raise ValueError("target argument can not be NaN")
+    if math.isnan(start):
+        raise  ValueError("start can not be NaN")
+    if maxiter <= 0:
+        raise ValueError('maxiter must be positive integer')
     
     xval = start
     yval = function(xval)
@@ -58,8 +64,11 @@ def newton(target, function, derivfun, start, bounds=None,
     
     n = 1
     while n <= maxiter:
-        print(n)
-        xval = xvals[-1] - function(xvals[-1])/derivfun(xvals[-1])
+        try:
+            xval = xvals[-1] - function(xvals[-1])/derivfun(xvals[-1])
+        except ValueError as error:
+            raise ValueError('Unable to devide: {0}'.format(error))
+        
         xvals.append(xval)
         fdiffs.append(target - function(xvals[-1]))
 
@@ -72,7 +81,8 @@ def newton(target, function, derivfun, start, bounds=None,
             return xvals, fdiffs
       
         n = n + 1
-    raise ValueError('Number of iterations {0} exceeded limit: {1}'.format(n,maxiter))
+        
+    raise ValueError('Number of iterations exceeded limit:{0}'.format(maxiter))
 
 
 def bisect(target, function, start=None, bounds=None,
@@ -86,7 +96,10 @@ def bisect(target, function, start=None, bounds=None,
     function : function
         the name of the Python function f
     start : float
-        the x-value to start looking at
+        the x-value to start looking at. If None, the mean of the upper and 
+        lower bounds shall be used. Subsequent steps shall always use the mean
+        of the active bounds. This input is used only when the initial bounds
+        have not been supplied.
     bounds : [float, float], optional
         the upper and lower bounds beyond which x shall not exceed
         the default is None
@@ -106,23 +119,61 @@ def bisect(target, function, start=None, bounds=None,
     fdiffs : [float,float,...]
         the set of (target - y) values at the xvals[]
     """        
-    # TODO: check input parameters
+    # check input parameters
+    if math.isnan(target):
+        raise ValueError("target argument can not be NaN")
+    if maxiter <= 0:
+        raise ValueError('maxiter must be positive integer')
+
+    if math.isnan(start) and bounds == None:
+        raise  ValueError("start and bounds can not be both NaN")        
+    
     xvals = []
     fdiffs = []
+    
+    #xval = start
+    #yval = function(xval)
+    
+    #xvals = [xval]
+    #fdiffs = [target - yval]    
+    
+    n = 1
+    while n <= maxiter:
+        #xval = xvals[-1] - function(xvals[-1])/derivfun(xvals[-1])
+        
+        xvals.append(xval)
+        fdiffs.append(target - function(xvals[-1]))
+
+        #if bounds is not None:#newton.__defaults__[0]:
+            #if xvals[-1] < min(bounds) or xvals[-1] > max(bounds):
+                #raise  ValueError('value x = {0} is Out of bounds {1}'.format(
+                    #xvals[-1],bounds))
+                
+        #if abs(xvals[-1]-xvals[-2])<tols[0] or abs(fdiffs[-1])<tols[1]:
+            #return xvals, fdiffs
+      
+        n = n + 1
+        
+    raise ValueError('Number of iterations exceeded limit:{0}'.format(maxiter))    
+    
+    
+    
     return xvals, fdiffs    
 
 target = 1.24
 y = lambda x: x**3 + 2*x**2 - 5
-dy = lambda x: x**2 + 4*x
+dy = lambda x: 3*x**2 + 4*x
 start = 5
 tols = [0.00001,0.010]
 maxiter = 10
-bounds = None#math.nan#[.0,10.0]
+bounds = None
 
 xvals, fdiffs = newton(target,y,dy,start,tols=tols,maxiter=maxiter)
 print(xvals, fdiffs)
 print(xvals[-1])
 print(fdiffs[-1])
 
-#xvals, fdiffs = bisect(target,y,dy,start,tols,maxiter)
-#print(xvals, fdiffs)
+start = math.nan
+
+xvals, fdiffs = bisect(target,y,start,tols=tols,maxiter=maxiter)
+print(xvals, fdiffs)
