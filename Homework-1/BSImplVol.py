0a43e511893f6107a42465b9d065795133a942e9
AlexShaGH <70188803+AlexShaGH@users.noreply.github.com>
2020-09-18 10:38:27 -0500

    final

--- Homework-1/BSImplVol.py
+++ Homework-1/BSImplVol.py
@@ -58,12 +58,45 @@ def bsimpvol(callput, S0, K, r, T, price, q=0.,
     numberofcalls : integer
         the number of calls to bsformula if roportCalls is set to True
     """
+    
+    # check input parameters for NaN
+    if math.isnan(callput):
+        raise ValueError("callput argument can not be NaN")
+    if math.isnan(K):
+        raise ValueError("Strike price can not be NaN")
+    if math.isnan(S0):
+        raise  ValueError("Underlying price can not be NaN")
+    if math.isnan(r):
+        raise  ValueError("Interest rate can not be NaN")
+    if math.isnan(T):
+        raise  ValueError("Time to expiration can not be NaN")
+    if math.isnan(price):
+        raise  ValueError("Volatility can not be NaN")
+    if math.isnan(q):
+        raise ValueError("Yield rate can not be NaN")
+    
+    # check values of input parameters
+    if abs(callput) != 1:
+        raise ValueError("Unable to parse option type {0}".format(callput))
+    if S0 <= 0:
+        raise ValueError("Underlying price can not be zero or negative")
+    if K <= 0:
+        raise ValueError("Strike price can not be zero or negative")
+    if T <= 0:
+        raise ValueError("Expiration time can not be zero or negative")
+    if price <= 0:
+        raise ValueError("Price can not be zero or negative")    
+    
+    
     xvals=[]
     fdiffs=[]
     impvol = math.nan
     numberofcalls = 0
-    start_sigma = 0.35
+    start_sigma = 1 # we need starting point
     
+    # we need to find sigma that would correspond to minimal difference
+    # between value of the option calculated with BS formula
+    # and real price of the option on the market
     f = lambda sigma: bsformula(callput,S0,K,r,T,sigma,q)[0] - price
     df = lambda sigma: bsformula(callput,S0,K,r,T,sigma,q)[2]    
     
--- Homework-1/test_BS.py
+++ Homework-1/test_BS.py
@@ -16,7 +16,6 @@ from Newton import newton
 from Bisect import bisect
 from BSImplVol import bsimpvol
 
-"""
 print('***** Test case 1: Checking calculated results against matlab\n')
 
 S0 = 100
@@ -397,7 +396,7 @@ try:
     print('bsformula results for Call option:\nPrice = {0:.4f}\n\
 Delta = {1:.4f}\nVega = {2:.4f}\n'.format(optionValue,delta,vega))
 except ValueError as error:    
-    print('bsformula returned ValueError: {0}'.format(error))"""
+    print('bsformula returned ValueError: {0}'.format(error))
     
     
     
@@ -407,74 +406,84 @@ dy = lambda x: 3*x**2 + 4*x
 start = 5
 tols = [0.00001,0.010]
 maxiter = 1000
-bounds = [0,3]#None
+bounds = [0,3]
 
+print('***** Test case 5: Root finding using')
 print("********** Newton's Method *************")
+print('Initial values:\n\
+y=x**3+2*x**2-5\n\
+dy=3*x**2+4*x\n\
+target={0}, start={1}\n\
+tolerances={2}, bounds={3}\n\
+maxiter={4}\n'.format(target,start,tols,bounds,maxiter))
 xvals, fdiffs = newton(target,y,dy,start,tols=tols,maxiter=maxiter)
 print(xvals, fdiffs)
 print('root = {0}'.format(xvals[-1]))
 print('error = {0}'.format(fdiffs[-1]))
-print('n = {0}'.format(len(xvals)))
+print('n = {0}\n'.format(len(xvals)))
 
-
+print('***** Test case 6: Root finding using')
 print("********** Bisect Method *************")
+print('Initial values:\n\
+y=x**3+2*x**2-5\n\
+target={0}, start={1}\n\
+tolerances={2}, bounds={3}\n\
+maxiter={4}\n'.format(target,start,tols,bounds,maxiter))
 xvals, fdiffs = bisect(target,y,start,tols=tols,maxiter=maxiter)
 print(xvals, fdiffs)
 print('root = {0}'.format(xvals[-1]))
 print('error = {0}'.format(fdiffs[-1]))
-print('n = {0}'.format(len(xvals)))    
+print('n = {0}\n'.format(len(xvals)))    
 
-
-S0 = 100
-K = 89
+callput = 1
+S0 = 110
+K = 100
 r = 0.05
 T = 0.5
-sigma = .5
 q = 0.025
+OptionPrice = 14
 
+print('***** Test case 7: Finding Implied volatility')
 print('Initial values:\n\
-S0={0}, K={1}, r={2}, T={3}, sigma={4}, q={5}\n'.format(S0,K,r,T,sigma,q))
+callput = {0}, S0 = {1}, K = {2}\n\
+r = {3}, T = {4}, q = {5}\n\
+OptionPrice = {6}\n'.format(callput,S0,K,r,T,q,OptionPrice))
 
-#mathlab results for the same initial values
-callValue_ml = 19.8433
-putValue_ml = 7.8881
-callDelta_ml = 0.6972
-putDelta_ml = -0.2903
-vega_ml = 24.0568
-
-print('Expected results:\nCall Price = {0}, Put Price = {1}\n\
-Call Delta = {2}, Put Delta = {3}\n\
-vega = {4}\n'.format(callValue_ml,putValue_ml,callDelta_ml,putDelta_ml,\
-vega_ml))
-
-callput = 1    
+print("********** Newton's Method *************")
 try:
-    optionValue, delta, vega = bsformula(callput,S0,K,r,T,sigma,q)
-    print('bsformula results for Call option:\nPrice = {0:.4f}\n\
-Delta = {1:.4f}\nVega = {2:.4f}\n'.format(optionValue,delta,vega))
-except ValueError as error:    
-    print('bsformula returned ValueError: {0}\n'.format(error))
-callput = -1  
+    impVol,callNum =  bsimpvol(callput,S0,K,r,T,OptionPrice,q,method='newton',reportCalls=True)
+    print('Implied volatility calculation using Newton method:')
+    print('implied volatility = {0},\nnumber of calls = {1}\n'.format(impVol,callNum))
+except ValueError as error:
+    print('bsimpvol returned ValueError: {0}\n'.format(error))
+
+print("********** Bisect Method *************")
 try:
-    optionValue, delta, vega = bsformula(callput,S0,K,r,T,sigma,q)
-    print('bsformula results for Put option:\nPrice = {0:.4f}\n\
-Delta = {1:.4f}\nVega = {2:.4f}\n'.format(optionValue,delta,vega))
-except ValueError as error:    
-    print('bsformula returned ValueError: {0}\n'.format(error))
+    impVol,callNum =  bsimpvol(callput,S0,K,r,T,OptionPrice,q,method='bisect',reportCalls=True)
+    print('Implied volatility calculation using Bisect method:')
+    print('implied volatility = {0},\nnumber of calls = {1}\n'.format(impVol,callNum))
+except ValueError as error:
+    print('bsimpvol returned ValueError: {0}\n'.format(error))
 
 callput = -1
-S0 = 100
-K = 89
-r = 0.05
-T = 0.5
-q = 0.025
-OptionPrice = 12
+print('Initial values:\n\
+callput = {0}, S0 = {1}, K = {2}\n\
+r = {3}, T = {4}, q = {5}\n\
+OptionPrice = {6}\n'.format(callput,S0,K,r,T,q,OptionPrice))
 
+print("********** Newton's Method *************")
+try:
+    impVol,callNum =  bsimpvol(callput,S0,K,r,T,OptionPrice,q,method='newton',reportCalls=True)
+    print('Implied volatility calculation using Newton method:')
+    print('implied volatility = {0},\nnumber of calls = {1}\n'.format(impVol,callNum))
+except ValueError as error:
+    print('bsimpvol returned ValueError: {0}\n'.format(error))
 
-impVol,callNum =  bsimpvol(callput,S0,K,r,T,OptionPrice,q,method='bisect',reportCalls=True)
-print('Implied volatility calculation using Bisect method:')
-print('implied volatility = {0},\nnumber of calls = {1}'.format(impVol,callNum))
+print("********** Bisect Method *************")
+try:
+    impVol,callNum =  bsimpvol(callput,S0,K,r,T,OptionPrice,q,method='bisect',reportCalls=True)
+    print('Implied volatility calculation using Bisect method:')
+    print('implied volatility = {0},\nnumber of calls = {1}\n'.format(impVol,callNum))
+except ValueError as error:
+    print('bsimpvol returned ValueError: {0}\n'.format(error))
 
-impVol,callNum =  bsimpvol(callput,S0,K,r,T,OptionPrice,q,method='newton',reportCalls=True)
-print('Implied volatility calculation using Newton method:')
-print('implied volatility = {0},\nnumber of calls = {1}'.format(impVol,callNum))
