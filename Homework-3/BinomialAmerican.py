435caac1aa72665ecfc9d2d027a79a99f8b3180f
AlexShaGH <70188803+AlexShaGH@users.noreply.github.com>
2020-10-18 21:05:49 -0500

    Q1 done
    Q1 done

--- Homework-3/BinomialAmerican.py
+++ Homework-3/BinomialAmerican.py
@@ -13,7 +13,6 @@ __author__ = "oshashkov"
 
 import numpy as np
 from numpy import exp,maximum,sqrt
-import matplotlib.pyplot as plt
 
 def binomialAmerican(S0, Ks, r, T, sigma, q, callputs, M):
     """ Calculates prices of American options in the Black-Scholes framework
@@ -93,8 +92,6 @@ def binomialAmerican(S0, Ks, r, T, sigma, q, callputs, M):
     
     df = exp(-(r-q)*dt) # discount factor accounting for dividents
     
-    N = M + 1 # number of leafs in recombining tree with M steps
-    
     # create initial tree to hold stock prices, S0 is a root node
     stock_prices = [np.array([S0])] # [[S0],[Su,Sd],[Suu,Sud,Sdd],...]
     
@@ -105,51 +102,59 @@ def binomialAmerican(S0, Ks, r, T, sigma, q, callputs, M):
         prev_branches = stock_prices[-1]
         st = np.concatenate((prev_branches*u,[prev_branches[-1]*d]))
         stock_prices.append(st)
-    #print(stock_prices)
-    
-    # traverse tree to compute spot price of the options
-    
-    payoffs = np.array([[0]*chain_lenght]*N,dtype='float32')
-    #print(payoffs)
-    print(stock_prices[-1])
-    print(Ks)
-    
-    # payofs at terminal nodes
+       
+    # payoffs at terminal nodes for each option's (strike, type) pair
+    payoffs = np.array([[0]*chain_lenght]*(M+1),dtype='float32')
     for i in range(chain_lenght):
-        if callputs[i] == 1:
-            payoffs[:,i] =np.maximum(0,stock_prices[M]-Ks[i])
-            #print(np.maximum(0,stock_prices[M]-Ks[i]))
-        else:
-            payoffs[:,i] = np.maximum(0,Ks[i]-stock_prices[M])
-            #print(np.maximum(0,Ks[i]-stock_prices[M]))
+        payoffs[:,i] = maximum(0, (stock_prices[M]-Ks[i]) if callputs[i]==1 
+                               else (Ks[i]-stock_prices[M]))
 
-    print(payoffs)
-                    
-    
-    #
-    
-    #
-    
-    
-        
-    prices = []
+    # traverse tree to compute spot prices of the options    
+    option_values = np.array([0]*chain_lenght, dtype='float32')
+    for j in range(chain_lenght):
+        payoffs_binary = payoffs[:,j]
+        for i in reversed(range(M)):
+            # The payoffs from NOT excercising option
+            payoffs_binary= (payoffs_binary[:-1] * qu + payoffs_binary[1:] * qd) * df
+            
+            # The payoffs from excercising option
+            payoffs_excersised = ((stock_prices[i]-Ks[j]) if callputs[j]==1 
+                         else (Ks[j] - stock_prices[i]))
+            
+            # Eliminate payoffs that do not make sense
+            payoffs_binary = maximum(payoffs_binary, payoffs_excersised)
+            
+        option_values[j] = payoffs_binary[0]
 
-    return prices
-
+    return option_values
 
 
 if __name__ == '__main__':
     # Problem 1 test:
-    #For example, your code should return a 3-element array of prices at t0 if I call
     S0 = 40
-    Ks = [30,35,40,45]
-    callputs = [-1,1,-1,-1]    
+    Ks = [30,35,40,45,50]
+    callputs = [1,1,1,1,1]    
     r = 0.05
-    T = 1.2
+    T = 1
     sigma =  0.15
     q = 0.01
-
-    M = 10
+    M = 12
+    
+    print('Parameters:\nS0 = {0}, Ks = {1}, r = {2}, T = {3},\n\
+Sigma = {4}, q = {5}, M = {6}\n'.format(S0,Ks,r,T,sigma,q,M))
+      
+    str_expected_results = '''
+    # binprice(S0, K, r, T, dt=1/12, sigma, callput, q)
+    # K = [30, 35, 40, 45, 50]
+    # Calls = [11.0887, 6.6616, 3.1301, 1.2023, 0.3478]
+    # Puts = [0.0241, 0.3715, 1.7746, 5.1145, 10.0000]
+'''
+    
+    print("Expected results from matlab:\n{0}\n".format(
+        str_expected_results))    
     
     option_prices = binomialAmerican(S0, Ks, r, T, sigma, q, callputs, M)
-    print(option_prices)
+    print('Calculated prices for call options:\n{0}\n'.format(option_prices))
+    callputs = [-1,-1,-1,-1,-1]
+    option_prices = binomialAmerican(S0, Ks, r, T, sigma, q, callputs, M)
+    print('Calculated prices for put options:\n{0}\n'.format(option_prices))
