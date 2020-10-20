f9265ce86a2b5e1716267222c8686f9009bf7ef1
AlexShaGH <70188803+AlexShaGH@users.noreply.github.com>
2020-10-20 10:11:09 -0500

    q2 done

--- Homework-3/FiniteDifferenceEuropean.py
+++ Homework-3/FiniteDifferenceEuropean.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# FiniteDifferenceEuropean.py - Finite dierence scheme for pricing European 
-# options in the Black-Scholes framework.
+# FiniteDifferenceEuropean.py - Finite difference scheme (explicit) for pricing
+# European options in the Black-Scholes framework
 # MSF 526
 # Illinois Institute of Technology
 # Homework 3
@@ -11,88 +11,130 @@
 
 __author__ = "oshashkov"
 
+import numpy as np
+from numpy import exp,maximum,linspace, interp
+
 def fdEuropean(callput, S0, K, r, T, sigma, q, M, N, S_max):
+    """ Calculates prices of European options in the Black-Scholes framework
+    using explicit finite difference schema
 
-    dS = Smax / float(M) # M+1 spatial values
-    dt = T / float(N) #N+1 time values
-    i_values = np.arange(M)
-    j_values = np.arange(N)
-    
+    Parameters
+    ----------
+    callput : int
+        1=call;-1=put, otherwise will raise ValueError
+    S0 : float
+        Underlying price
+    K : float
+        Strike price of the option
+    r : float
+        risk-free interest rate expressed as a fraction
+        (i.e. r = 0.1 stands for 10%)
+    T : float
+        the expiration date of the option
+        defines the pitch of the grid for the time axis
+    sigma : float
+        the constant volatility  
+    q : float
+        Yield, continuous return rate on the underlying expressed as a fraction
+        (i.e. q = 0.05 stands for 5%)        
+    M : int
+        defines the pitch of the grid for the price axis
+    N : int
+        defines the pitch of the grid for the time axis
+    S_max : float
+        unrelaisticialy large price that security is unlikely to reach
+        defines the pitch of the grid for the price axis
 
-    a = 0.5*dt*((sigma**2) *(i_values**2) -
-                              r*i_values)
-    b = 1 - dt*((sigma**2) * (i_values**2) + r)
-    c = 0.5*dt*((sigma**2) *(i_values**2) +
-                              r*i_values)
+    Raises
+    ------
+    ValueError
+        when passed parameters are invalid
+
+    Returns
+    -------
+    float
+        calculated option value
+    """
     
+    # check parameters
+    if not abs(callput) == 1:
+        raise ValueError("Invalid value of callput, must be 1 or -1")    
+    if np.isnan(S0):
+        raise  ValueError("Underlying price can not be NaN")
+    if np.isnan(K):
+        raise  ValueError("Strike price can not be NaN")        
+    if np.isnan(sigma):
+        raise  ValueError("Volatility can not be NaN")
+    if np.isnan(M):
+        raise  ValueError("M can not be NaN")
+    if np.isnan(N):
+        raise  ValueError("N can not be NaN")        
+    if np.isnan(T):
+        raise  ValueError("Time to expiration can not be NaN")
+
+    # check the values of input parameters
+    if S0 <= 0:
+        raise ValueError("Underlying price can not be zero or negative")
+    if S_max <= 0:
+        raise ValueError("S_max can not be zero or negative")
+    if K <= 0:
+        raise ValueError("Strike price can not be zero or negative")        
+    if sigma <= 0:
+        raise ValueError("Volatility can not be zero or negative")
+    if T <= 0:
+        raise ValueError("Expiration time can not be zero or negative")
+    if M < 1:
+        raise ValueError("M has to be 1 or more")
+    if N < 1:
+        raise ValueError("N has to be 1 or more")        
+
+    dt = T / float(N) # N + 1 time values
+    #dS = S_max / float(M) # not used in explicit method
+
     # create datastructures
-    grid = np.zeros(shape=(M+1, N+1)) #M+1 by N+1 (full) grid 
-    boundary_conds = np.linspace(0, Smax, M+1) # M+1 payoff values (time boundary condition: last columns)
-    
-    # call payoffs at final value at spatial B.C.s
-    if callput:
-            grid[:, -1] = np.maximum(
-                boundary_conds - K, 0)
-            grid[-1, :-1] = np.maximum(Smax - self.K, 0) * \
-                                 np.exp(-r *
-                                        self.dt *
-                                        (self.N-self.j_values))
-        else:
-            grid[:, -1] = \
-                np.maximum(K-boundary_conds, 0)
-            grid[0, :-1] = self.K * \
-                               np.exp(-self.r *
-                                      self.dt *
-                                      (self.N-self.j_values))
-    
-    for j in reversed(self.j_values):
-            for i in range(self.M)[1:]:
-              self.grid[i,j] = self.a[i]*self.grid[i-1,j+1] +\
-                                 self.b[i]*self.grid[i,j+1] + \
-                                 self.c[i]*self.grid[i+1,j+1]
-    
-    
-    return interp(S0, boundary_conds, grid[:, 0])
-
-
-def fdEuropean(callput, S0, K, r, T, sigma, q, M, N, S_max):
-    dS = Smax / float(M) # M+1 spatial values
-    dt = T / float(N) #N+1 time values
-    
     i_values = np.arange(M)
     j_values = np.arange(N)
+    grid = np.zeros((M + 1, N + 1))
+    boundary_conds = linspace(0, S_max, M + 1)
     
+    # setup boundary conditions
+    if callput == 1:
+        grid[:, -1] = maximum(0,boundary_conds - K)
+        grid[-1, :-1] = (S_max-K)*exp(-(r-q)*dt*(N-j_values))
+    else:
+        grid[:, -1] = maximum(0,K-boundary_conds)
+        grid[0, :-1] = (K-S_max)*exp(-(r-q)*dt*(N-j_values))
+        
+    # setup coefficients
+    a = 0.5*dt*i_values*((sigma**2)*i_values - r + q)
+    b = 1 - dt*((sigma**2)*(i_values**2) + r)
+    c = 0.5*dt*i_values*((sigma**2)*i_values + r - q)
+    #print('a = \n{0}'.format(a))
+    #print('b = \n{0}'.format(b))
+    #print('c = \n{0}'.format(c))
+        
+    # traverse grid
+    for j in reversed(j_values):
+        for i in range(M)[2:]:
+            grid[i,j]=a[i]*grid[i-1,j+1]+b[i]*grid[i,j+1]+c[i]*grid[i+1,j+1]
 
-    a = 0.5*dt*((sigma**2) *(i_values**2) -
-                              r*i_values)
-    b = 1 - dt*((sigma**2) * (i_values**2) + r)
-    c = 0.5*dt*((sigma**2) *(i_values**2) +
-                              r*i_values)    
+    #print('grid = \n{0}'.format(grid))
     
-	matval = np.zeros((M+1,N+1))
-	vetS = np.linspace(0,S_max,M+1)
-	
-    veti = np.array(range(M+1))
-    vetj = np.array(range(N+1))
-    
-	if callput == 1:
-		matval[:,N] = np.maximum(vetS-K,0)
-		matval[0,:] = 0
-		matval[M,:] = S_max*np.exp(-q*dt*(N-vetj)) - K*np.exp(-r*dt*(N-vetj))
-	else:
-		matval[:,N] = np.maximum(K-vetS,0)
-		matval[0,:] = K*np.exp(-r*dt*(N-vetj))
-		matval[M,:] = 0
-	a = 0.5*dt*(sigma**2*veti - (r-q))*veti
-	b = 1 - dt*(sigma**2*veti**2 + (r-q))
-	c = 0.5*dt*(sigma**2*veti + (r-q))*veti
-	for j in range(N-1,-1,-1):
-		for i in range(1,M):
-			matval[i,j] = a[i]*matval[i-1,j+1] + b[i]*matval[i,j+1]+ \
-			c[i]*matval[i+1,j+1]
-	return np.exp(-q*T)*np.interp(S0, vetS, matval[:,0])
+    return interp(S0, boundary_conds, grid[:, 0])            
 
 
 if __name__ == '__main__':
-    # Problem 1 test:
-    pass
+    # Problem 2 test:
+    callput = 1
+    S0 = 40
+    K = 30
+    r = 0.05
+    T = 1
+    sigma =  0.15
+    q = 0.01
+    M = 120
+    N = 1000
+    S_max = 200
+    
+    results = fdEuropean(callput, S0, K, r, T, sigma, q, M, N, S_max)
+    print(results)
